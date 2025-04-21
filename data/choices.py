8c2f61551523f75025368a05c4505d0f828c39ed
CZ <private-email>
2025-04-21 15:59:56 +0200

    add enums for user choices

---
+++ data/choices.py
@@ -0,0 +1,10 @@
+from enum import Enum, auto
+
+class COMPOSITE_ATTRIBUTE_CHOICE(Enum):
+    KEEP_COMPOSITE = auto()
+    KEEP_SIMPLE_ATTRIBUTES = auto()
+
+class RELATIONSHIP_CHOICE(Enum):
+    RELANTIONSHIP_TABLE = auto()
+    MERGE_INTO_SINGLE_TABLE = auto()
+    ADD_FOREIGN_KEY = auto()
