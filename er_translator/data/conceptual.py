ab55f5d0373722810481473846f59d46a2fd7a9c
CZ <private-email>
2025-11-04 21:32:14 +0100

    Add method for creating deep copy

--- er_translator/data/conceptual.py
+++ er_translator/data/conceptual.py
@@ -45,6 +45,10 @@ class Cardinality():
             max_cardinality = MaximumCardinality.MANY
 
         return Cardinality(min_cardinality, max_cardinality)
+    
+    def deep_copy(self) -> "Cardinality":
+        """Create a deep copy of this Cardinality."""
+        return Cardinality(self._min_cardinality, self._max_cardinality)
 
 class Entity:
     def __init__(self, name: str):
@@ -97,6 +101,33 @@ class Entity:
             if attribute.name == attribute_name:
                 self._attributes.remove(attribute)
                 break
+    
+    def remove_identifier(self, identifier_name: str):
+        for identifier in self._identifiers:
+            if identifier.name == identifier_name:
+                self._identifiers.remove(identifier)
+                break
+    
+    def deep_copy(self) -> "Entity":
+        """Create a deep copy of this Entity."""
+        entity_copy = Entity(self._name)
+        
+        # Deep copy identifiers
+        for identifier in self._identifiers:
+            entity_copy.add_identifier(identifier.deep_copy())
+        
+        # Deep copy attributes
+        for attribute in self._attributes:
+            entity_copy.add_attribute(attribute.deep_copy())
+        
+        # Copy strong entity reference (string, immutable)
+        entity_copy._strong_entity = self._strong_entity
+        
+        # Deep copy hierarchy if it exists
+        if self._hierarchy:
+            entity_copy._hierarchy = self._hierarchy.deep_copy()
+        
+        return entity_copy
 
 class Hierarchy:
     def __init__(self, hierarchy_completeness: HierarchyCompleteness, hierarchy_disjointness: HierarchyDisjointness):
@@ -118,6 +149,13 @@ class Hierarchy:
     
     def add_child(self, child: str):
         self.children.append(child)
+    
+    def deep_copy(self) -> "Hierarchy":
+        """Create a deep copy of this Hierarchy."""
+        hierarchy_copy = Hierarchy(self._hierarchy_completeness, self._hierarchy_disjointness)
+        # Copy all children (strings are immutable, so we can just copy the list)
+        hierarchy_copy._children = self._children.copy()
+        return hierarchy_copy
 
 class Relationship:
     def __init__(self, name: str, entity_from: str, entity_to: str, cardinality_from: Cardinality, cardinality_to: Cardinality):
@@ -151,9 +189,40 @@ class Relationship:
     @property
     def attributes(self):
         return self._attributes
+    
+    def set_name(self, name: str):
+        self._name = name
 
     def add_attribute(self, attribute: "Attribute"):
         self._attributes.append(attribute)
+
+    def set_cardinality_from(self, cardinality: Cardinality):
+        self._cardinality_from = cardinality
+    
+    def set_cardinality_to(self, cardinality: Cardinality):
+        self._cardinality_to = cardinality
+    
+    def set_entity_from(self, entity_from: str):
+        self._entity_from = entity_from
+    
+    def set_entity_to(self, entity_to: str):
+        self._entity_to = entity_to
+    
+    def deep_copy(self) -> "Relationship":
+        """Create a deep copy of this Relationship."""
+        relationship_copy = Relationship(
+            self._name,
+            self._entity_from,
+            self._entity_to,
+            self._cardinality_from.deep_copy(),
+            self._cardinality_to.deep_copy()
+        )
+        
+        # Deep copy attributes
+        for attribute in self._attributes:
+            relationship_copy.add_attribute(attribute.deep_copy())
+        
+        return relationship_copy
 
 
 class Attribute:
@@ -173,6 +242,10 @@ class Attribute:
     @property
     def is_unique(self):
         return self._is_unique
+    
+    def deep_copy(self) -> "Attribute":
+        """Create a deep copy of this Attribute."""
+        return Attribute(self._name, self._cardinality.deep_copy(), self._is_unique)
 
 class CompositeAttribute(Attribute):
     def __init__(self, name: str, cardinality: Cardinality, is_unique: bool):
@@ -185,4 +258,14 @@ class CompositeAttribute(Attribute):
     
     def add_simple_attribute(self, simple_attribute: Attribute):
         self._simple_attributes.append(simple_attribute)
+    
+    def deep_copy(self) -> "CompositeAttribute":
+        """Create a deep copy of this CompositeAttribute."""
+        composite_copy = CompositeAttribute(self._name, self._cardinality.deep_copy(), self._is_unique)
+        
+        # Deep copy all simple attributes
+        for simple_attr in self._simple_attributes:
+            composite_copy.add_simple_attribute(simple_attr.deep_copy())
+        
+        return composite_copy
     
